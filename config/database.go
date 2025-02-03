5ba9657a1a76bd67c00b729fd457e7adb58d86ba
Aben <5195693+rushairer@users.noreply.github.com>
2025-02-03 22:35:02 +0800

    feat: users

---
+++ config/database.go
@@ -0,0 +1,13 @@
+package config
+
+import "github.com/rushairer/sso/utils"
+
+var MySQLDSN = utils.GetEnv(
+	"MYSQL_DSN",
+	"root:123456@tcp(localhost:3306)/sso?multiStatements=true",
+)
+
+var MigrationsPath = utils.GetEnv(
+	"MIGRATIONS_PATH",
+	"file://./migrations",
+)
--- frontend/web/main.go
+++ frontend/web/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rushairer/sso/config"
@@ -15,14 +21,39 @@ func main() {
 			log.Println("web crashed, error:", err)
 		}
 	}()
+	var err error
+
+	db, err := sql.Open("mysql", config.MySQLDSN)
+	if err != nil {
+		log.Println("db error:", err)
+	}
+	migrations(db)
 
 	log.Println("starting...")
 	server := gin.Default()
 	bootstrap.SetupServer(server)
 
 	log.Println("running...")
-	err := server.Run(fmt.Sprintf(":%s", config.WebServerPort))
+	err = server.Run(fmt.Sprintf(":%s", config.WebServerPort))
 	if err != nil {
 		log.Println(err)
+	}
+}
+
+func migrations(db *sql.DB) {
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		log.Println("mysql error:", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		config.MigrationsPath,
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		log.Println("migrations error:", err)
+	}
+	if err = m.Up(); err != nil {
+		log.Println("migrations up error:", err)
 	}
 }
